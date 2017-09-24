5167b29ac1c7838166fde9de785e463d7bb9ba53
Donald Whyte <donaldwhyte0@gmail.com>
2017-09-24 14:13:13 +0100

    Got Tensorflow training working with refactored code

--- src/model.py
+++ src/model.py
@@ -1,12 +1,19 @@
 import datetime
 import logging
+import math
+import os
+import time
+from typing import Callable, List, Optional
+
 import tensorflow as tf
 from tensorflow.contrib import layers
 # `rnn` module temporarily in contrib. It's moving back to code in TF 1.1.
 from tensorflow.contrib import rnn
-from typing import Callable, List, Optional
 
-from text import ALPHABET_SIZE, print_learning_learned_comparison
+from text import (ALPHABET_SIZE,
+                  print_learning_learned_comparison,
+                  rnn_minibatch_generator,
+                  SequenceData)
 
 _logger = logging.getLogger(__name__)
 
@@ -18,6 +25,7 @@ class RNNTextModel:
 
     def __init__(self,
                  sequence_length: int,
+                 batch_size: int,
                  gru_internal_size: int,
                  num_hidden_layers: int,
                  stats_log_dir: str):
@@ -25,26 +33,33 @@ class RNNTextModel:
 
         TODO: document all inputs
         """
+        # Mark time this text model was initially created
+        self._timestamp = str(math.trunc(time.time()))
+
+        # Store hyperparameters
         self._sequence_length = sequence_length
+        self._batch_size = batch_size
 
         # Define RNN inputs.
+        X = tf.placeholder(tf.uint8, [None, None], name='X')
         self._inputs = {
             'learning_rate': tf.placeholder(tf.float32, name='learning_rate'),
             'batch_size': tf.placeholder(tf.int32, name='batch_size'),
             # Dimensions: [ batch_size, sequence_length ]
-            'X': tf.placeholder(tf.uint8, [None, None], name='X')
+            'X': X,
             # Dimensions: [ batch_size, sequence_length, ALPHABET_SIZE ]
-            'Xo': tf.one_hot(self._inputs['X'], ALPHABET_SIZE, 1.0, 0.0)
+            'Xo': tf.one_hot(X, ALPHABET_SIZE, 1.0, 0.0)
         }
 
         # Define expected RNN outputs. This is used for training.
         # This is the same sequence as the input sequence, but shifted by 1
         # since we are trying to predict the next character.
-        self.expected_outputs = {
+        Y_exp =  tf.placeholder(tf.uint8, [None, None], name='Y_exp')
+        self._expected_outputs = {
             # Dimensions: [ batch_size, sequence_length ]
-            'Y':  = tf.placeholder(tf.uint8, [None, None], name='Y_exp')
+            'Y': Y_exp,
             # Dimensions: [ batch_size, sequence_length, ALPHABET_SIZE ]
-            'Yo':  = tf.one_hot(self._out, ALPHABET_SIZE, 1.0, 0.0)
+            'Yo': tf.one_hot(Y_exp, ALPHABET_SIZE, 1.0, 0.0)
         }
 
         # Define internal/hidden RNN layers. The RNN is composed of a certain
@@ -54,16 +69,17 @@ class RNNTextModel:
         # a single cell.
         self._cells = [
             rnn.GRUCell(gru_internal_size) for _ in range(num_hidden_layers)]
-        self._multicell = rnn.MultiRNNCell(cells, state_is_tuple=True)
+        self._multicell = rnn.MultiRNNCell(self._cells, state_is_tuple=True)
 
-        # When using state_is_tuple=True, you must use multicell.zero_state
-        # to create a tuple of  placeholders for the input states (one state
+        # When using state_is_tuple=True, you must use multicell.zero_state to
+        # to create a tuple of placeholders for the input states (one state
         # per layer).
         #
         # When executed using session.run(self._zero_state), this also returns
         # the correctly shaped initial zero state to use when starting your
         # training loop.
-        self._zero_state = multicell.zero_state(batch_size, dtype=tf.float32)
+        self._zero_state = self._multicell.zero_state(
+            self._batch_size, dtype=tf.float32)
 
         # Using `dynamic_rnn` means Tensorflow "performs fully dynamic
         # unrolling" of the network. This is faster than compiling the full
@@ -80,13 +96,13 @@ class RNNTextModel:
         # H:  [ batch_size, gru_internal_size * num_hidden_layers ]
         # H is the last state in the sequence.
         Yr, H = tf.nn.dynamic_rnn(
-            multicell,
-            Xo,
+            self._multicell,
+            self._inputs['Xo'],
             dtype=tf.float32,
             initial_state=self._zero_state)
 
         # Do this just to give H a identifiable name
-        H = tf.identity(H, name='H')
+        self._H = tf.identity(H, name='H')
 
         # Softmax layer implementation:
         # Flatten the first two dimensions of the output. This performs the
@@ -100,8 +116,9 @@ class RNNTextModel:
         # weights and biases are shared across unrolled time steps. From the
         # readout point of view, a value coming from a cell or a minibatch is
         # the same thing.
-        Ylogits = layers.linear(Yflat, ALPHABET_SIZE)                      # [ batch_size x sequence_length, ALPHABET_SIZE ]
-        Yflat_ = tf.reshape(Yo_, [-1, ALPHABET_SIZE])                      # [ batch_size x sequence_length, ALPHABET_SIZE ]
+        Ylogits = layers.linear(Yflat, ALPHABET_SIZE)                  # [ batch_size x sequence_length, ALPHABET_SIZE ]
+        Yflat_ = tf.reshape(                                           # [ batch_size x sequence_length, ALPHABET_SIZE ]
+            self._expected_outputs['Yo'], [-1, ALPHABET_SIZE])
         self._loss = tf.nn.softmax_cross_entropy_with_logits(          # [ batch_size x sequence_length ]
             logits=Ylogits, labels=Yflat_)
         self._loss = tf.reshape(                                       # [ batch_size, sequence_length ]
@@ -110,25 +127,34 @@ class RNNTextModel:
         Y = tf.argmax(Yo, 1)                                           # [ batch_size x sequence_length ]
         Y = tf.reshape(Y, [self._inputs['batch_size'], -1], name='Y')  # [ batch_size, sequence_length ]
 
+        # TODO: comment why define these here
+        self._actual_outputs = {'Y': Y}
+
+        # TODO: comment on why these are here
+        self._sequence_loss = tf.reduce_mean(self._loss, 1)
+        self._batch_loss = tf.reduce_mean(self._sequence_loss)
+        self._batch_accuracy = tf.reduce_mean(
+            tf.cast(
+                tf.equal(
+                    self._expected_outputs['Y'],
+                    tf.cast(self._actual_outputs['Y'], tf.uint8)),
+                tf.float32))
+
+        # TODO: explain what this is
         self._train_step = tf.train.AdamOptimizer().minimize(self._loss)
 
         self._build_statistics(stats_log_dir)
         self._initialise_tf_session()
 
     def _build_statistics(self, stats_log_dir: str):
-        sequence_loss = tf.reduce_mean(loss, 1)
-        batch_loss = tf.reduce_mean(sequence_loss)
-        batch_accuracy = tf.reduce_mean(
-            tf.cast(tf.equal(Y_, tf.cast(Y, tf.uint8)), tf.float32))
-        loss_summary = tf.summary.scalar('batch_loss', batch_loss)
-        acc_summary = tf.summary.scalar('batch_accuracy', batch_accuracy)
-
+        loss_summary = tf.summary.scalar('batch_loss', self._batch_loss)
+        acc_summary = tf.summary.scalar('batch_accuracy', self._batch_accuracy)
         self._summaries = tf.summary.merge([loss_summary, acc_summary])
-        self._stats_summary_writer = tf.summary.FileWriter(
-            os.path.join(stats_log_dir, f'{timestamp}-training'))
 
-        self. = tf.summary.FileWriter(
-            os.path.join(stats_log_dir, f'{timestamp}-validation'))
+        self._stats_summary_writer = tf.summary.FileWriter(
+            os.path.join(stats_log_dir, f'{self._timestamp}-training'))
+        self._validation_stats_writer = tf.summary.FileWriter(
+            os.path.join(stats_log_dir, f'{self._timestamp}-validation'))
 
     def _initialise_tf_session(self):
         initalised_vars = tf.global_variables_initializer()
@@ -137,11 +163,10 @@ class RNNTextModel:
         self._initialised = True
 
     def run_training_loop(self,
-                          training_text: List[int],
+                          data: SequenceData,
                           num_epochs: int,
                           learning_rate: float,
                           display_every_n_batches: int,
-                          batch_size: int,
                           checkpoint_dir: str,
                           on_step_complete: Callable[[int], None],
                           should_stop: Callable[[int], bool]):
@@ -149,13 +174,15 @@ class RNNTextModel:
 
         TODO: document all args
         """
+        training_text, test_text, file_index = data
+
         # We display information on the currently trained model's accuracy
         # after training every N batches. Each batch corresponds to a single
         # step of the training loop. Each batch processes a number of sequences
         # which are X characters in length. Therefore, a step's size is defined
         # as the number of input sequences processed in a batch multiplied by
         # _length_ of each input sequence.
-        step_size = batch_size * self._sequence_length
+        step_size = self._batch_size * self._sequence_length
         display_every_n_batches = display_every_n_batches * step_size
 
         # Create direcftory that stores checkpoints of the trained model.
@@ -169,16 +196,16 @@ class RNNTextModel:
         # Initial zero input state (a tuple).
         input_state = self._session.run(self._zero_state)
 
-        batch_sequencer = txt.rnn_minibatch_sequencer(
-            training_text, batch_size, sequence_length, nb_epochs=num_epochs)
+        batch_sequencer = rnn_minibatch_generator(
+            training_text, self._batch_size, self._sequence_length, num_epochs)
         step = 0
         for x, y_, epoch in batch_sequencer:
             # Train on one minibatch.
             feed_dict = {
                 self._inputs['X']: x,
-                self._inputs['Y_']: y_,
+                self._expected_outputs['Y']: y_,
                 self._inputs['learning_rate']: learning_rate,
-                self._inputs['batch_size']: batch_size
+                self._inputs['batch_size']: self._batch_size
             }
 
             # This is how you add the input state to feed dictionary when
@@ -194,44 +221,62 @@ class RNNTextModel:
             # values.
             for i, v in enumerate(self._zero_state):
                 feed_dict[v] = input_state[i]
-            # This is the call that actuall runs the training batch.
+            # This is the call that actually trains on the batch.
             _, y, output_state, summary = self._session.run(
-                [self._train_step, Y, H, self._summaries],
+                [
+                    self._train_step,
+                    self._actual_outputs['Y'],
+                    self._H,
+                    self._summaries
+                ],
                 feed_dict=feed_dict)
 
             # Save training data for Tensorboard
-            self._summary_writer.add_summary(summary, step)
+            self._stats_summary_writer.add_summary(summary, step)
 
-            # Display a visual validation of progress (every 50 batches)
+            # Run a validation step every 50 batches.
+            # The validation text should be a single sequence but that's too
+            # slow (1s per 1024 chars!), so we cut it up and batch the pieces
+            # (slightly inaccurate).
+            #
+            # tested: validating with 5K sequences instead of 1K is only
+            # slightly more accurate, but a lot slower.
             if step % display_every_n_batches == 0:
                 # We don't use dropout for running the validation!
-                feed_dict = {X: x, Y_: y_, batchsize: batch_size}
+                feed_dict = {
+                    self._inputs['X']: x,
+                    self._expected_outputs['Y']: y_,
+                    self._inputs['batch_size']: self._batch_size
+                }
                 for i, v in enumerate(self._zero_state):
                     feed_dict[v] = input_state[i]
-                y, l, bl, acc = self._session.run(
+                y, losses, batch_loss, batch_accuracy = self._session.run(
                     [
-                        self._expected_outputs['Y'],
+                        self._actual_outputs['Y'],
                         self._sequence_loss,
                         self._batch_loss,
                         self._batch_accuracy
                     ],
                     feed_dict=feed_dict)
+                epoch_size = len(training_text) // (
+                    self._batch_size * self._sequence_length)
                 print_learning_learned_comparison(
                     x[:5],
                     y,
-                    l,
+                    losses,
                     file_index,
-                    bl,
-                    acc,
+                    batch_loss,
+                    batch_accuracy,
                     epoch_size,
                     step,
                     epoch)
+                # TODO: do proper validation!
 
             # Save a checkpoint (every 500 batches)
             if step // 10 % display_every_n_batches == 0:
                 saver.save(
                     self._session,
-                    f'checkpoints/rnn_train_{timestamp}',
+                    f'checkpoints/rnn_train_{self._timestamp}',
                     global_step=step)
 
             on_step_complete(step)
--- src/text.py
+++ src/text.py
@@ -1,7 +1,9 @@
 import glob
 import logging
 import os
-from typing import List, Tuple
+from typing import Generator, List, Tuple
+
+import numpy as np
 
 _logger = logging.getLogger(__name__)
 
@@ -10,15 +12,15 @@ _logger = logging.getLogger(__name__)
 ALPHABET_SIZE = 98
 
 
-def read_data_files(
-        glob_pattern: str,
-        validation: bool=True) -> Tuple[List[int], List[int], List[dict]]:
+# TODO: explain what this is
+SequenceData = Tuple[List[int], List[int], List[dict]]
+
+
+def read_data_files(glob_pattern: str, validation: bool=True) -> SequenceData:
     '''Read data files found with the specified `glob_pattern`.
 
     If `validation` is set to `True`, then set aside last file as validation
     data.
-
-    TODO: what this returns
     '''
     encoded_text = []
     file_index = []
@@ -26,7 +28,7 @@ def read_data_files(
     for fname in files:
         with open(fname, 'r') as f:
             start = len(encoded_text)
-            encoded_text.extend(_encode_text(shaketext.read()))
+            encoded_text.extend(encode_text(f.read()))
             end = len(encoded_text)
             file_index.append({
                 'start': start,
@@ -66,14 +68,13 @@ def read_data_files(
     # pick the smallest
     num_files_in_training = min(
         num_files_in_10percent_of_chars,
-        num_files_in_firstx_90kb,
+        num_files_in_first_90kb,
         num_files_in_20percent_of_files)
 
     if num_files_in_training == 0 or not validation:
         training_chars_cutoff = len(encoded_text)
     else:
-        training_chars_cutoff =
-            file_index[-num_files_in_training]['start']
+        training_chars_cutoff = file_index[-num_files_in_training]['start']
     training_text = encoded_text[:training_chars_cutoff]
     validation_text = encoded_text[training_chars_cutoff:]
 
@@ -91,6 +92,121 @@ def decode_to_text(chars: List[int], avoid_tab_and_lf: bool=False) -> str:
         [_convert_to_alphabet(ch, avoid_tab_and_lf) for ch in chars])
 
 
+# A training batch consists of (X, Y, epoch), which are the input feature
+# matrix, expected output matrix and epoch number respectively.
+Batch = Tuple[np.matrix, np.matrix, int]
+
+
+def rnn_minibatch_generator(data: List[int],
+                            batch_size: int,
+                            sequence_length: int,
+                            num_epochs: int) -> Generator[Batch, None, None]:
+    """Construct a generator that yield training batches.
+
+    Divides `data` into batches of sequences so that all the sequences in
+    one batch continue in the next batch. This is a generator that will keep
+    returning batches until the input data has been seen `num_epochs` times.
+
+    Sequences are continued even between epochs, apart from one, the one
+    corresponding to the end of `data`. The remainder at the end of `data` that
+    does not fit in an full batch is ignored.
+
+    `batch_size` is the number of sequences to put in a each yielded `Batch`.
+
+    `sequence_length` is the length of each training sequence (in chars). This
+    must match the unroll size of the RNN you're feeding these batches into.
+    """
+    data = np.array(data)
+    data_len = data.shape[0]
+    # Using (data_len-1) because we must provide for the sequence shifted by
+    # one too.
+    num_batches = (data_len - 1) // (batch_size * sequence_length)
+    if num_batches < 1:
+        raise RuntimeError(
+            'Not enough data, even for a single batch. Try using a smaller '
+            'batch_size.')
+    rounded_data_len = num_batches * batch_size * sequence_length
+    xdata = np.reshape(
+        data[0:rounded_data_len],
+        [batch_size, num_batches * sequence_length])
+    ydata = np.reshape(
+        data[1:rounded_data_len + 1],
+        [batch_size, num_batches * sequence_length])
+
+    for epoch in range(num_epochs):
+        for batch in range(num_batches):
+            x = xdata[:, batch * sequence_length:(batch + 1) * sequence_length]
+            y = ydata[:, batch * sequence_length:(batch + 1) * sequence_length]
+            # If we've reached the end of the training data, we wrap around to
+            # the start of the training data. We don't stop here because we
+            # might have more epochs to run.
+            x = np.roll(x, -epoch, axis=0)
+            y = np.roll(y, -epoch, axis=0)
+            yield x, y, epoch
+
+
+class ProgressBar:
+    """Text mode progress bar.
+
+    Usage:
+            p = Progress(30)
+            p.step()
+            p.step()
+            p.step(start=True) # to restart form 0%
+
+    The progress bar displays a new header at each restart.
+    """
+    def __init__(self,
+                 steps_to_finish: int,
+                 size_of_bar_in_chars: int=100,
+                 header_message: str=""):
+        """TODO."""
+        self._steps_to_finish = steps_to_finish
+        self._size_of_bar_in_chars = size_of_bar_in_chars
+        self._header_message = header_message
+
+        # Use `()` to get the iterator from the generator
+        self.p = self.__start_progress(self._steps_to_finish)()
+        self._header_printed = False
+
+    def step(self, reset: bool=False):
+        """TODO."""
+        if reset:
+            self.__init__(
+                self._steps_to_finish,
+                self._size_of_bar_in_chars,
+                self._header_message)
+        if not self._header_printed:
+            self.__print_header()
+        next(self.p)
+
+    def __print_header(self):
+        print()
+        format_string = (
+            "0%{: ^" + str(self._size_of_bar_in_chars - 6) + "}100%")
+        print(format_string.format(self._header_message))
+        self._header_printed = True
+
+    def __start_progress(self,
+                         steps_to_finish: int) -> Generator[int, None, None]:
+        def print_progress():
+            # Bresenham's algorithm. Yields the number of dots printed.
+            # This will always print 100 dots in max invocations.
+            dx = self._steps_to_finish
+            dy = self._size_of_bar_in_chars
+            d = dy - dx
+            for x in range(self._steps_to_finish):
+                k = 0
+                while d >= 0:
+                    print('=', end="", flush=True)
+                    k += 1
+                    d -= dx
+                d += dy
+                yield k
+
+        return print_progress
+
+
 def print_learning_learned_comparison(X,
                                       Y,
                                       losses,
@@ -103,6 +219,8 @@ def print_learning_learned_comparison(X,
     """Display utility for printing learning statistics.
 
     TODO: document type of each thing.
+
+    TODO: better explain what this is printing
     """
     print()
     # epoch_size in number of batches
@@ -113,12 +231,12 @@ def print_learning_learned_comparison(X,
         index_in_epoch = index % (epoch_size * batch_size * sequence_len)
         decx = decode_to_text(X[k], avoid_tab_and_lf=True)
         decy = decode_to_text(Y[k], avoid_tab_and_lf=True)
-        bookname = find_book(index_in_epoch, file_index)
+        fname = _find_file(index_in_epoch, file_index)
         # min 10 and max 40 chars
-        formatted_bookname = "{: <10.40}".format(bookname)
-        epoch_string = "{:4d}".format(index) + " (epoch {}) ".format(epoch)
-        loss_string = "loss: {:.5f}".format(losses[k])
-        print_string = epoch_string + formatted_bookname + " │ {} │ {} │ {}"
+        formatted_fname = '{: <10.40}'.format(fname)
+        epoch_string = '{:4d}'.format(index) + ' (epoch {}) '.format(epoch)
+        loss_string = 'loss: {:.5f}'.format(losses[k])
+        print_string = epoch_string + formatted_fname + ' │ {} │ {} │ {}'
         print(print_string.format(decx, decy, loss_string))
         index += sequence_len
     # box formatting characters:
@@ -129,11 +247,11 @@ def print_learning_learned_comparison(X,
     # ┴ \u2534
     # ┌ \u250C
     # ┐ \u2510
-    format_string = f'└{{:─^{len(epoch_string))}}}'
-    format_string += f'{{:─^{len(formatted_bookname))}}}'
-    format_string += f'┴{{:─^{len(decx) + 2)}}}'
-    format_string += f'┴{{:─^{len(decy) + 2)}}}'
-    format_string += f'┴{{:─^{len(loss_string))}}}┘'
+    format_string = f'└{{:─^{len(epoch_string)}}}'
+    format_string += f'{{:─^{len(formatted_fname)}}}'
+    format_string += f'┴{{:─^{len(decx) + 2}}}'
+    format_string += f'┴{{:─^{len(decy) + 2}}}'
+    format_string += f'┴{{:─^{len(loss_string)}}}┘'
     footer = format_string.format(
         'INDEX', 'BOOK NAME', 'TRAINING SEQUENCE',
         'PREDICTED SEQUENCE', 'LOSS')
@@ -147,6 +265,11 @@ def print_learning_learned_comparison(X,
     print(f'TRAINING STATS: {stats}')
 
 
+def _find_file(char_index: int, file_index: List[dict]) -> str:
+    return next(
+        f['name'] for f in file_index
+        if (f['start'] <= char_index < f['end']))
+
 
 def _convert_from_alphabet(ch: str) -> int:
     """Encode character `ch` to an integer.
@@ -159,17 +282,17 @@ def _convert_from_alphabet(ch: str) -> int:
         * 97-122 lower-case letters
         *  123-126 more punctuation
     """
-    if a == 9:
+    if ch == 9:
         return 1
-    if a == 10:
+    if ch == 10:
         return 127 - 30  # LF
-    elif 32 <= a <= 126:
-        return a - 30
+    elif 32 <= ch <= 126:
+        return ch - 30
     else:
         return 0  # unknown
 
 
-def _convert_to_alphabet(ch: int, avoid_tab_and_lf: bool=False) -> int:
+def _convert_to_alphabet(ch: int, avoid_tab_and_lf: bool=False) -> chr:
     """Decode an encoded character `ch` to a string chartacter.
 
     What each input integer will be converted to:
@@ -179,6 +302,10 @@ def _convert_to_alphabet(ch: int, avoid_tab_and_lf: bool=False) -> int:
         * 2 to 96 = 36 to 126 ASCII codes
         * 97 = LF (linefeed)
     """
+    return chr(_convert_to_alphabet_impl(ch, avoid_tab_and_lf))
+
+
+def _convert_to_alphabet_impl(ch: int, avoid_tab_and_lf: bool=False) -> int:
     if ch == 1:
         return 32 if avoid_tab_and_lf else 9  # space instead of TAB
     if ch == 127 - 30:
